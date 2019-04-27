468eb9cc24c9cd2d516e08fd4e913a3e906b98ae
astrahaninn <astrahaninn@gmail.com>
2019-04-27 13:20:51 +0300

    fileNotFoundError warning has been added

--- config.py
+++ config.py
@@ -1,14 +1,22 @@
 import yaml
+import log
 
 
 class Config:
     def __init__(self, path="configs/config.yaml"):
         self.path = path
+        log.setup()
+        self.logger = log.logging.getLogger(__name__)
 
     def load(self):
         """Load config file
         :return: dict {'parameter_name': value}
         """
-        with open(self.path, 'r') as ymlfile:
-            cfg = yaml.load(ymlfile)
+        try:
+            with open(self.path, 'r') as ymlfile:
+                cfg = yaml.load(ymlfile)
+        except FileNotFoundError:
+            self.logger.warning("Config file %s hasn't been found" % self.path)
+            cfg = None
+
         return cfg
